68fa5744cc164c0c8680d248384f9d8fd71c8175
DESKTOP-U5HC8V0\PC <gbrain@naver.com>
2026-09-25 09:56:07 +0900

    after movie and rm big size file

--- util/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# 현재 실행 파일의 폴더 경로를 파이썬 라이브러리 검색 경로에 강제로 추가 [1]
-current_dir = os.path.dirname(os.path.abspath(__file__))
-if current_dir not in sys.path:
-    sys.path.append(current_dir)
